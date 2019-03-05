f6778cff0ad0407182077d4a93aaa5dcba6eb305
Hao, Hayden (Hai Quan) <HHAO01@beckman.com>
2019-03-05 18:18:18 +0800

    Use Custom Azure AD to do authentication.

--- src/BeckmanCoulter.BookStore/Startup.cs
+++ src/BeckmanCoulter.BookStore/Startup.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BeckmanCoulter.BookStore.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace BeckmanCoulter.BookStore
 {
@@ -34,13 +33,43 @@ namespace BeckmanCoulter.BookStore
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
 
+      services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+        .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+
+      services.Configure<OpenIdConnectOptions>(AzureADDefaults.OpenIdScheme, options =>
+      {
+        options.Authority = options.Authority + "/v2.0/";
+
+        // Per the code below, this application signs in users in any Work and School
+        // accounts and any Microsoft Personal Accounts.
+        // If you want to direct Azure AD to restrict the users that can sign-in, change 
+        // the tenant value of the appsettings.json file in the following way:
+        // - only Work and School accounts => 'organizations'
+        // - only Microsoft Personal accounts => 'consumers'
+        // - Work and School and Personal accounts => 'common'
+
+        // If you want to restrict the users that can sign-in to only one tenant
+        // set the tenant value in the appsettings.json file to the tenant ID of this
+        // organization, and set ValidateIssuer below to true.
+
+        // If you want to restrict the users that can sign-in to several organizations
+        // Set the tenant value in the appsettings.json file to 'organizations', set
+        // ValidateIssuer, above to 'true', and add the issuers you want to accept to the
+        // options.TokenValidationParameters.ValidIssuers collection
+        options.TokenValidationParameters.ValidateIssuer = false;
+      });
+
       services.AddDbContext<ApplicationDbContext>(options =>
           options.UseSqlServer(
               Configuration.GetConnectionString("DefaultConnection")));
-      services.AddDefaultIdentity<IdentityUser>()
-          .AddEntityFrameworkStores<ApplicationDbContext>();
 
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      services.AddMvc(options =>
+      {
+        var policy = new AuthorizationPolicyBuilder()
+          .RequireAuthenticatedUser()
+          .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+      }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
