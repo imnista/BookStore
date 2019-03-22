0a1112acc36a82f3d57ba1ef36b64178d264f598
Cass Fan <WFAN@beckman.com>
2019-03-22 15:14:53 +0800

    refactor startup.cs

--- src/BeckmanCoulter.BookStore/Startup.cs
+++ src/BeckmanCoulter.BookStore/Startup.cs
@@ -1,41 +1,40 @@
 ﻿using BeckmanCoulter.BookStore.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace BeckmanCoulter.BookStore
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration, IHostingEnvironment environment)
     {
-        public Startup(IConfiguration configuration, IHostingEnvironment environment)
-        {
-            Configuration = configuration;
-            _environment = environment;
-        }
+      Configuration = configuration;
+      _environment = environment;
+    }
 
-        public IConfiguration Configuration { get; }
-        private readonly IHostingEnvironment _environment;
+    public IConfiguration Configuration { get; }
+    private readonly IHostingEnvironment _environment;
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options =>
+      {
+              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+              options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
 
       services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
         .AddAzureAD(options => Configuration.Bind("AzureAd", options));
@@ -63,76 +62,76 @@ namespace BeckmanCoulter.BookStore
         options.TokenValidationParameters.ValidateIssuer = false;
       });
 
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
+      //services.AddDbContext<ApplicationDbContext>(options =>
+      //    options.UseSqlServer(
+      //        Configuration.GetConnectionString("DefaultConnection")));
 
-            // Use sqlite DbContext
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")
-                ));
+      // Use sqlite DbContext
+      services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")
+          ));
 
-            services.AddMvc(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+      services.AddMvc(options =>
+      {
+        var policy = new AuthorizationPolicyBuilder()
+                  .RequireAuthenticatedUser()
+                  .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+      }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            #region Add AddSerilog
+      #region Add AddSerilog
 
-            //Read configuration
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(_environment.ContentRootPath)
-                .AddJsonFile("appsettings.json", true, true);
+      //Read configuration
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(_environment.ContentRootPath)
+          .AddJsonFile("appsettings.json", true, true);
 
-            if (!string.IsNullOrWhiteSpace(_environment.EnvironmentName))
-            {
-                builder = builder.AddJsonFile($"appsettings.{_environment.EnvironmentName}.json", true);
-            }
+      if (!string.IsNullOrWhiteSpace(_environment.EnvironmentName))
+      {
+        builder = builder.AddJsonFile($"appsettings.{_environment.EnvironmentName}.json", true);
+      }
 
-            builder = builder.AddEnvironmentVariables();
-            var configuration = builder.Build();
+      builder = builder.AddEnvironmentVariables();
+      var configuration = builder.Build();
 
-            //build logger
-            var logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .Enrich.FromLogContext()
-                .CreateLogger();
-            Log.Logger = logger;
+      //build logger
+      var logger = new LoggerConfiguration()
+          .ReadFrom.Configuration(configuration)
+          .Enrich.FromLogContext()
+          .CreateLogger();
+      Log.Logger = logger;
 
-            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+      services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
 
-            #endregion Add AddSerilog
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseAuthentication();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      #endregion Add AddSerilog
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseAuthentication();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
 }
