fccc490b6b0f20bbc455c11dc6fb8433d64cab94
unikisi <unikisi@163.com>
2019-03-23 22:49:09 +0800

    update

--- src/BeckmanCoulter.BookStore/Controllers/BooksController.cs
+++ src/BeckmanCoulter.BookStore/Controllers/BooksController.cs
@@ -18,18 +18,18 @@ using SendGrid.Helpers.Mail;
 
 namespace BeckmanCoulter.BookStore.Controllers
 {
-    public class BooksController : Controller
-    {
-        private readonly ApplicationDbContext _context;
-        private readonly IHostingEnvironment _env;
-        private readonly IMailQueueService _mailQueueService;
+  public class BooksController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+    private readonly IHostingEnvironment _env;
+    private readonly IMailQueueService _mailQueueService;
 
-        public BooksController(ApplicationDbContext context, IHostingEnvironment env, IMailQueueService mailQueueService)
-        {
-            _context = context;
-            _env = env;
-            _mailQueueService = mailQueueService;
-        }
+    public BooksController(ApplicationDbContext context, IHostingEnvironment env, IMailQueueService mailQueueService)
+    {
+      _context = context;
+      _env = env;
+      _mailQueueService = mailQueueService;
+    }
 
     // GET: Books
     public async Task<IActionResult> Index()
@@ -52,22 +52,34 @@ namespace BeckmanCoulter.BookStore.Controllers
         return NotFound();
       }
 
-      book.Image = _env.WebRootPath + @"\bookfiles\" + book.Image;
+      book.Image = @"\bookfiles\" + book.Image;
       return View(book);
     }
 
-        // GET: Books/Create
-        public IActionResult Create()
-        {
-            var from = new EmailAddress("lfu01@beckman.com", "Lynn");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("lfu01@beckman.com", "Lynn");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var mail = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            _mailQueueService.SendMessage(mail);
-            return View();
-        }
+    [HttpPost, ActionName("Details")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Details(Book model)
+    {
+      if (ModelState.IsValid)
+      {
+
+      }
+
+      return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Books/Create
+    public IActionResult Create()
+    {
+      var from = new EmailAddress("lfu01@beckman.com", "Lynn");
+      var subject = "Sending with SendGrid is Fun";
+      var to = new EmailAddress("lfu01@beckman.com", "Lynn");
+      var plainTextContent = "and easy to do anywhere, even with C#";
+      var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+      var mail = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+      _mailQueueService.SendMessage(mail);
+      return View();
+    }
 
     // POST: Books/Create
     // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
--- src/BeckmanCoulter.BookStore/ViewModels/BookViewModels.cs
+++ src/BeckmanCoulter.BookStore/ViewModels/BookViewModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 
 namespace BeckmanCoulter.BookStore.ViewModels
@@ -9,5 +11,11 @@ namespace BeckmanCoulter.BookStore.ViewModels
     public int Quantity { get; set; }
     public string Description { get; set; }
     public IFormFile Files { get; set; }
+  }
+
+  public class DetailPostModel
+  {
+    [Required]
+    public Guid BookId { get; set; }
   }
 }
