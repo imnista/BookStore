3c388e500c1b94e3351531ba8cd210d0078077ab
Hendry <hqi@beckman.com>
2019-03-27 17:23:04 +0800

    Modified   src/BeckmanCoulter.BookStore/Models/BookInfo.cs

--- src/BeckmanCoulter.BookStore/Models/BookInfo.cs
+++ src/BeckmanCoulter.BookStore/Models/BookInfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BeckmanCoulter.BookStore.Models
+﻿namespace BeckmanCoulter.BookStore.Models
 {
     public class BookInfo
     {
