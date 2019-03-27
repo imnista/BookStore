2bd8ca712b98f3db1d4909a21df083982d42e955
Jeff (Shujie) Wang <SWANG016@beckman.com>
2019-03-27 15:01:37 +0800

    Add utility for Api request

---
+++ src/BeckmanCoulter.BookStore/Helper/BookInfoHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml;
+using BeckmanCoulter.BookStore.Models;
+
+namespace BeckmanCoulter.BookStore.Helper
+{
+    public class BookInfoHelper
+    {
+        private const string UrlTemplate = @"https://book.douban.com/subject_search?search_text={0}";
+
+        private ApiHelper apiHelper = new ApiHelper();
+
+        public async Task<bool> GetBookInfo(string name)
+        {
+            BookInfo bi = new BookInfo();
+
+            //     string url = string.Format(UrlTemplate, name);
+            //     string result = await apiHelper.GetHttpMethodAsync(url,null);
+            //web
+            //XmlDocument xDoc = new XmlDocument();
+            //xDoc.InnerText = result;
+            return true;
+        }
+    }
+}
